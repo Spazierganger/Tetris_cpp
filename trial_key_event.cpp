a35bf2041d9279202c68980c17291f1be9902d46
Spazierganger <1049653012@qq.com>
2020-03-24 19:58:21 +0100

    a little modification

--- trial_key_event.cpp
+++ trial_key_event.cpp
@@ -3,20 +3,23 @@
 //
 
 #include <ncurses.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
     initscr();
-    cbreak();
+    raw();
     noecho();
     refresh();
     printw("press any key\n");
     char c = getch();
     while(c != 'q')
     {
-        refresh();
-        printw("Press q for exit.\n");
+        printw("Press q to exit!\n");
         c = getch();
+        refresh();
     }
     endwin();
     return 0;
